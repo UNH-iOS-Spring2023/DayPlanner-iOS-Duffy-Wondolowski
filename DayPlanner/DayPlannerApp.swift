0ca894c6ab978879a65cd6a0c316c20696714526
orionduffy <45501204+orionduffy@users.noreply.github.com>
2023-03-05 18:36:47 -0500

    Initial Commit
    The initial commit

---
+++ DayPlanner/DayPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayPlannerApp.swift
+//  DayPlanner
+//
+//  Created by Orion Duffy on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DayPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
