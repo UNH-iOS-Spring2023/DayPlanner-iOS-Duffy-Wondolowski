1c95bd6858aea35f0e0a149efe0b61ab5513cf6f
orionduffy <45501204+orionduffy@users.noreply.github.com>
2023-03-05 19:19:24 -0500

    Added bottom bar
    Also added the initial files for the other pages, but no work has been done on them yet

---
+++ DayPlanner/Planner/Planner.swift
@@ -0,0 +1,20 @@
+//
+//  Planner.swift
+//  DayPlanner
+//
+//  Created by Orion Duffy on 3/5/23.
+//
+
+import SwiftUI
+
+struct Planner: View {
+    var body: some View {
+        Text("Planner Page")
+    }
+}
+
+struct Planner_Previews: PreviewProvider {
+    static var previews: some View {
+        Planner()
+    }
+}
