f2605c5786db0f789c9d73b3985b57646ccefe78
Spencer Wondolowski <spencerwondo@gmail.com>
2023-03-05 21:08:06 -0500

    Added the two views SignIn and SignUp

---
+++ DayPlanner/SignInView.swift
@@ -0,0 +1,21 @@
+//
+//  SignInView.swift
+//  DayPlanner
+//
+//  Created by Spencer Wondolowski on 3/5/23.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
---
+++ DayPlanner/SignUpView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpView.swift
+//  DayPlanner
+//
+//  Created by Spencer Wondolowski on 3/5/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
