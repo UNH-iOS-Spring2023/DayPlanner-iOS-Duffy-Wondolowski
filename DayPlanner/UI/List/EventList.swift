bb4598a9946389837b265add5dfb36691c715cd2
Spencer Wondolowski <spencerwondo@gmail.com>
2023-03-19 11:17:10 -0400

    Renamed list to EventList

--- DayPlanner/UI/List/EventList.swift
+++ DayPlanner/UI/List/EventList.swift
@@ -7,8 +7,9 @@
 
 import SwiftUI
 
-struct List: View {
+struct EventList: View {
     @EnvironmentObject private var app: AppVariables
+    @State var draggedEvent: Event? = nil
     
     var body: some View {
         //Once again the button is largely copied from the professor
@@ -22,7 +23,7 @@ struct List: View {
                     Text("+")
                         .background(
                             RoundedRectangle(cornerRadius: 25.0)
-                                .fill(CustomColor.darkGreen)
+                                .fill(Color(.systemBlue))
                                 .frame(width:50, height:50)
                         )
                         .font(.system(.largeTitle))
@@ -34,28 +35,39 @@ struct List: View {
             }
         }
         
+        let list = ScrollView {
+            VStack (spacing: 10) {
+                ForEach(app.eventList, id: \.self.id) {
+                    (event: Event) in VStack { ListItem(event: event) }
+                        .opacity(event == draggedEvent ? 0.1 : 1)
+                        .onDrag {
+                            draggedEvent = event
+                            return CustomProvider(ended: {draggedEvent = nil})
+                        }
+                        .onDrop(of: [.item], delegate: ScrollDropDelegate(droppedOnEvent: event, eventList: $app.eventList, draggedEvent: $draggedEvent))
+                }
+            }
+            .animation(.default, value: app.eventList)
+        }
+        
         if (app.isEventEdit) {
             EventEdit()
         } else {
             ZStack {
-                
-                CustomColor.background
-                    .ignoresSafeArea(.all)
-                
-                ZStack {
-                    Text("List Page")
-                        .padding(5)
-                        .foregroundColor(.white)
-                    button
-                }
+                list
+                button
             }
+//            .onDrop(of: [.item], delegate: DropOutsideDelegate(draggedEvent: $draggedEvent))
         }
     }
+    
 }
 
 struct List_Previews: PreviewProvider {
     static var previews: some View {
-        List()
-            .environmentObject(ErrorChecking.app)
+        EventList()
+            .environmentObject(AppVariables())
     }
 }
+
+
