404c92e8f145d33ede7cce332526d5b5e5499c4f
orionduffy <45501204+orionduffy@users.noreply.github.com>
2023-04-02 19:25:23 -0400

    Old event deletion & Loading in splash screen
    If it is a new day from when you last logged in, it now properly deletes the data from Firestore. The loading of data was also shifted to the splash screen, so there is no longer a visible delay between the end of the splash screen and the events appearing on decent hardware

--- DayPlanner/ContentView.swift
+++ DayPlanner/ContentView.swift
@@ -9,42 +9,9 @@ import SwiftUI
 import FirebaseAuth
 import FirebaseFirestore
 
-class AppVariables: ObservableObject {
-    @Published var selectedTab: Int = 0
-    //Determines whether the app goes to the edit event page when the list is selected
-    @Published var isEventEdit: Bool = false
-    @Published var eventList: [Event] = []
-    @Published var selectedEvent: Event? = nil
-    @Published var user: User = User()
-    
-    @Published var uid: String? = nil
-    
-    ///Check to ensure that there is no overlap in time between a new event to be added and the already existing events
-    /// - Parameter oldEvent: If the new event is overwriting an old event, the old event is passed so it can be excluded from the check
-    /// - Returns: True if there is no overlap, false if there is
-    func checkEventOverlap(newEvent: Event, oldEvent: Event? = nil) -> Bool {
-        //True means there is no overlap
-        return ErrorChecking.checkEventOverlap(eventList: eventList, newEvent: newEvent, oldEvent: oldEvent)
-    }
-    
-    ///Check if the day has changed since the last time events were cleared, and clear non-recurring events if it has
-    func checkTime() {
-        let calendar = Calendar(identifier: .gregorian)
-        let date = calendar.startOfDay(for: Date())
-//        print("Day Planner: start of day: \(date)")
-//        print("Day Planner: eventsLastCleared: \(user.eventsLastCleared)")
-        if user.eventsLastCleared < date {
-            eventList = eventList.filter { event in
-                event.recurring != 0
-            }
-        }
-    }
-}
-
 struct ContentView: View {
-    @StateObject var app = AppVariables()
+    @EnvironmentObject var app: AppVariables
     @Environment(\.scenePhase) private var scenePhase
-    let db = Firestore.firestore()
     
     var body: some View {
         
@@ -57,63 +24,7 @@ struct ContentView: View {
             .environmentObject(app)
         }
         .onAppear() {
-            app.uid = Auth.auth().currentUser?.uid
             
-            if app.uid == nil {
-//                Persistence.load{ (userResult, eventResult) in
-////                                print("Full load user result: \(userResult)")
-////                                print("Full load event result: \(eventResult)")
-//                    switch userResult {
-//                    case .failure(let error):
-//                        print("Error loading users: \(error.localizedDescription)")
-//                    case .success(let user):
-//                        app.user = user
-//                    }
-//
-//                    switch eventResult {
-//                    case .failure(let error):
-//                        print("Error loading events: \(error.localizedDescription)")
-//                    case .success(let events):
-//                        app.eventList = events
-//                    }
-//                }
-//                do {
-//                    let eventData = UserDefaults.standard.value(forKey: "eventList")
-//                    app.eventList = try
-//                    Firestore.Decoder().decode([Event].self, from: eventData ?? [])
-//
-//                    let userData = UserDefaults.standard.value(forKey: "user")
-//                    app.user = try
-//                    Firestore.Decoder().decode(User.self, from: userData ?? User())
-//                } catch {
-//                    print("Error saving data: \(error.localizedDescription)")
-//                }
-            } else {
-                app.eventList = []
-                db.collection("Users/\(app.uid!)/events")
-                    .getDocuments() { (events, err) in
-                        if let err = err {
-                            print("Error getting events: \(err)")
-                        } else {
-                            for event in events!.documents {
-                                do {
-                                    app.eventList
-                                        .append(try event.data(as: Event.self))
-                                } catch {
-                                    print("Error converting db event: \(error)")
-                                }
-                            }
-                        }
-                    }
-                db.collection("Users").document(app.uid!)
-                    .getDocument(as: User.self) { result in
-                        switch result {
-                        case .success(let user): app.user = user
-                        case.failure(let error):
-                            print("Error getting user: \(error)")
-                        }
-                }
-            }
             app.checkTime()
         }
         .onChange(of: scenePhase) { phase in
